06b9798e7631f40489517d6a0e3a1047b04f9476
Haim Daniel <hdaniel@redhat.com>
2016-03-24 16:23:57 +0200

    First working version:  -Outputs formatted messages with (id, subj, date)  -Object oriented

---
+++ gmail_client_api.py
@@ -0,0 +1,162 @@
+from __future__ import print_function
+
+import os
+import base64
+import email
+
+import httplib2
+from apiclient import errors
+from apiclient import discovery
+
+import oauth2client
+from oauth2client import client
+from oauth2client import tools
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/gmail-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Gmail API Python Quickstart'
+
+
+class GmailMessage(object):
+    def __init__(self, email_message):
+        self.email_message = email_message
+        self.headers = self.email_message['payload']['headers']
+        self.parse_message_params()
+
+    def parse_message_header(self, hdr):
+        if hdr['name'] == 'Subject':
+            self.subject = hdr['value']
+        elif hdr['name'] == 'Date':
+            self.date = hdr['value']
+
+    def parse_message_params(self):
+        self.id = self.email_message['id']
+        for h in self.headers:
+            self.parse_message_header(h)
+
+    def __str__(self):
+        return u','.join((self.id, self.subject, self.date)).encode(
+            'utf-8').strip()
+
+
+class GmailClientApi(object):
+    def __init__(self, flags):
+        self.flags = flags
+        self.credentials = self.get_credentials()
+        self.http = self.credentials.authorize(httplib2.Http())
+        self.service = discovery.build(serviceName='gmail', version='v1',
+                                       http=self.http)
+
+    def get_credentials(self):
+        """Gets valid user credentials from storage.
+
+        If nothing has been stored, or if the stored credentials are invalid,
+        the OAuth2 flow is completed to obtain the new credentials.
+
+        Returns:
+            Credentials, the obtained credential.
+        """
+        home_dir = os.path.expanduser('~')
+        credential_dir = os.path.join(home_dir, '.credentials')
+        if not os.path.exists(credential_dir):
+            os.makedirs(credential_dir)
+        credential_path = os.path.join(credential_dir,
+                                       'gmail-python-quickstart.json')
+
+        store = oauth2client.file.Storage(credential_path)
+        credentials = store.get()
+        if not credentials or credentials.invalid:
+            flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+            flow.user_agent = APPLICATION_NAME
+            if self.flags:
+                credentials = tools.run_flow(flow, store, self.flags)
+            else:  # Needed only for compatibility with Python 2.6
+                credentials = tools.run(flow, store)
+            print('Storing credentials to ' + credential_path)
+        return credentials
+
+    def get_message(self, user_id, msg_id):
+        """Get a Message with given ID.
+
+        Args:
+          user_id: User's email address. The special value "me"
+          can be used to indicate the authenticated user.
+          msg_id: The ID of the Message required.
+
+        Returns:
+          A Message.
+        """
+        try:
+            message = self.service.users().messages().get(userId=user_id,
+                                                          id=msg_id).execute()
+            # print('Message snippet: %s' % message['snippet'])
+
+            return GmailMessage(message)
+
+        except errors.HttpError, error:
+            print('An error occurred: %s' % error)
+
+    def get_mime_message(self, user_id, msg_id):
+        """Get a Message and use it to create a MIME Message.
+
+        Args:
+          service: Authorized Gmail API service instance.
+          user_id: User's email address. The special value "me"
+          can be used to indicate the authenticated user.
+          msg_id: The ID of the Message required.
+
+        Returns:
+          A MIME Message, consisting of data from Message.
+        """
+        try:
+            message = self.service.users().messages().get(
+                userId=user_id,
+                id=msg_id,
+                format='raw').execute()
+
+            print('Message snippet: %s' % message['snippet'])
+
+            msg_str = base64.urlsafe_b64decode(message['raw'].encode('ASCII'))
+
+            mime_msg = email.message_from_string(msg_str)
+
+            return mime_msg
+        except errors.HttpError, error:
+            print('An error occurred: %s' % error)
+
+    def list_messages_matching_query(self, user_id, query=''):
+        """List all Messages of the user's mailbox matching the query.
+
+        Args:
+          service: Authorized Gmail API service instance.
+          user_id: User's email address. The special value "me"
+          can be used to indicate the authenticated user.
+          query: String used to filter messages returned.
+          Eg.- 'from:user@some_domain.com' for Messages from a particular sender.
+
+        Returns:
+          List of Messages that match the criteria of the query. Note that the
+          returned list contains Message IDs, you must use get with the
+          appropriate ID to get the details of a Message.
+        """
+        try:
+            response = self.service.users().messages().list(userId=user_id,
+                                                            q=query).execute()
+            messages = []
+            if 'messages' in response:
+                messages.extend(response['messages'])
+
+            while 'nextPageToken' in response:
+                page_token = response['nextPageToken']
+                response = self.service.users().messages().list(
+                    userId=user_id,
+                    q=query,
+                    pageToken=page_token).execute()
+                messages.extend(response['messages'])
+
+            return messages
+
+        except errors.HttpError, error:
+            print('An error occurred: %s' % error)
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from __future__ import print_function
-import httplib2
-import os
 
-from apiclient import discovery
-import oauth2client
-from oauth2client import client
+# from apiclient import discovery
+
 from oauth2client import tools
+
+from gmail_client_api import GmailClientApi
 
 try:
     import argparse
@@ -13,56 +12,22 @@ try:
 except ImportError:
     flags = None
 
-# If modifying these scopes, delete your previously saved credentials
-# at ~/.credentials/gmail-python-quickstart.json
-SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
-CLIENT_SECRET_FILE = 'client_secret.json'
-APPLICATION_NAME = 'Gmail API Python Quickstart'
 
+def print_formatted_msgs(client, msg_id_list):
+    for msg_id in msg_id_list:
+        msg = client.get_message(user_id='me', msg_id=msg_id)
+        print(msg)
 
-def get_credentials():
-    """Gets valid user credentials from storage.
-
-    If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth2 flow is completed to obtain the new credentials.
-
-    Returns:
-        Credentials, the obtained credential.
-    """
-    home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir, '.credentials')
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,
-                                   'gmail-python-quickstart.json')
-
-    store = oauth2client.file.Storage(credential_path)
-    credentials = store.get()
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-        flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else: # Needed only for compatibility with Python 2.6
-            credentials = tools.run(flow, store)
-        print('Storing credentials to ' + credential_path)
-    return credentials
 
 def main():
-    credentials = get_credentials()
-    http = credentials.authorize(httplib2.Http())
-    service = discovery.build('gmail', 'v1', http=http)
+    gclient = GmailClientApi(flags)
+    # a list of [ {"id": "153a833d1dbee17e", "threadId": "153a833d1dbee17e"}]
+    messages = gclient.list_messages_matching_query(
+        user_id='me',
+        query='to: tlv-food-arrivals')
 
-    results = service.users().labels().list(userId='me').execute()
-    labels = results.get('labels', [])
-
-    if not labels:
-        print('No labels found.')
-    else:
-      print('Labels:')
-      for label in labels:
-        print(label['name'])
-
+    msg_ids = [m['id'] for m in messages]
+    print_formatted_msgs(gclient, msg_ids)
 
 if __name__ == '__main__':
     main()
